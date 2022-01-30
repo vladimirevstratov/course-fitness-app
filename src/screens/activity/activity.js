5a16b5057c80784c4f4574764fa0d6a8e8aa92ae
Vladimir <vladimir.evstratov@list.ru>
2022-01-31 00:08:16 +0300

    Styles for activities list

--- src/screens/activity/activity.js
+++ src/screens/activity/activity.js
@@ -1,5 +1,6 @@
 import React from "react"
 import {Text, FlatList, View} from "react-native"
+import styles from "./activity.styles"
 
 const activities = [
     {
@@ -37,17 +38,35 @@ const activities = [
 const Activity = () => {
     const renderItem = ({item}) => {
         return (
-            <View>
-                <Text>{item.name}</Text>
-                <Text>{item.date}</Text>
+            <View style={styles.itemContainer}>
+                <View style={styles.image} />
+                <View style={styles.infoContainer}>
+                    <View>
+                        <Text style={styles.name}>{item.name}</Text>
+                        <Text style={styles.date}>{item.date}</Text>
+                    </View>
+                    <View style={styles.actionsContainer}>
+                        <Text style={styles.action}>Edit</Text>
+                        <Text style={styles.action}>Delete</Text>
+                    </View>
+                </View>
             </View>
         )
     }
 
+    const renderSeparator = () => {
+        return <View style={styles.separator} />
+    }
+
     return (
-        <View>
+        <View style={styles.container}>
             <Text>List of activities</Text>
-            <FlatList data={activities} renderItem={renderItem} keyExtractor={item => item.id} />
+            <FlatList 
+                data={activities}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+                ItemSeparatorComponent={renderSeparator}
+            />
         </View>
     )
 }
