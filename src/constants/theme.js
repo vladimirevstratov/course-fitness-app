9ae138acf84ad2d5b1f3850b254b62651eaea15d
Vladimir <vladimir.evstratov@list.ru>
2022-05-22 01:01:07 +0300

    add eslint

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,5 +1,5 @@
-export const RICH_BLACK_COLOR = '#343434'
-export const ALICE_BLUE_COLOR = '#F9FCFF'
-export const MEDIUM_STATE_BLUE_COLOR = '#736CED'
-export const WHITE_COLOR = '#FFFFFF'
-export const GREY_COLOR = '#A5A5A5'
+export const RICH_BLACK_COLOR = '#343434';
+export const ALICE_BLUE_COLOR = '#F9FCFF';
+export const MEDIUM_STATE_BLUE_COLOR = '#736CED';
+export const WHITE_COLOR = '#FFFFFF';
+export const GREY_COLOR = '#A5A5A5';
--- src/screens/activity/activity.styles.js
+++ src/screens/activity/activity.styles.js
@@ -2,46 +2,46 @@ import { StyleSheet } from "react-native";
 import { GREY_COLOR, RICH_BLACK_COLOR } from "../../constants/theme";
 
 const styles = StyleSheet.create({
-    container: {
-        paddingTop: 40,
-        paddingLeft: 21,
-        paddingRight: 16,
-    },
-    itemContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    infoContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-    },
-    name: {
-        color: RICH_BLACK_COLOR,
-        fontSize: 18,
-    },
-    date: {
-        color: GREY_COLOR,
-        fontSize: 14,
-        marginTop: 5,
-    },
-    image: {
-        width: 96,
-        height: 85,
-        backgroundColor: 'gray',
-        marginRight: 21,
-        borderRadius: 13,
-    },
-    actionsContainer: {
-        flexDirection: 'row',
-    },
-    action: {
-        marginLeft: 11,
-    },
-    separator: {
-       height: 18, 
-    },
-})
+  container: {
+    paddingTop: 40,
+    paddingLeft: 21,
+    paddingRight: 16,
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  infoContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  name: {
+    color: RICH_BLACK_COLOR,
+    fontSize: 18,
+  },
+  date: {
+    color: GREY_COLOR,
+    fontSize: 14,
+    marginTop: 5,
+  },
+  image: {
+    width: 96,
+    height: 85,
+    backgroundColor: 'gray',
+    marginRight: 21,
+    borderRadius: 13,
+  },
+  actionsContainer: {
+    flexDirection: 'row',
+  },
+  action: {
+    marginLeft: 11,
+  },
+  separator: {
+    height: 18,
+  },
+});
 
 export default styles;
